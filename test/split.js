3134d1f2c94ca0b3b5fab656fe93453464a3dea4
Hershal Bhave <hershal.bhave@gmail.com>
2016-08-20 17:30:17 -0500

    add testing framework and split test

---
+++ test/split.js
@@ -0,0 +1,15 @@
+const assert = require('chai').assert;
+const _ = require('lodash');
+
+const split = require('../split');
+
+describe('join tests', function () {
+  it('should split a simple string', function () {
+    const input = 'hello friendly world';
+    const output = 'hello\nfriendly\nworld';
+
+    const splitted = split(input);
+    assert(splitted === output,
+           `split: ${splitted} != out: ${output}`);
+  });
+});
