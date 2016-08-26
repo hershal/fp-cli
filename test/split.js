68407e5c440a942f5995eb61b7d5200659424676
Hershal Bhave <hershal.bhave@gmail.com>
2016-08-25 22:55:31 -0500

    test: add split tests

--- test/split.js
+++ test/split.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const _ = require('lodash');
+const exec = require('child_process').execSync;
 
 const split = require('../split');
 
@@ -7,9 +8,26 @@ describe('split tests', function () {
   it('should split a simple string', function () {
     const input = 'hello friendly world';
     const output = 'hello\nfriendly\nworld';
-
     const splitted = split(input);
-    assert(splitted === output,
-           `split: ${splitted} != out: ${output}`);
+    ensure(splitted, output);
+  });
+
+  it('should split a more complicated string', function () {
+    const input = 'hello:this:is:dog';
+    const output = 'hello dog';
+    const splitted = split(input, {inputDelimeter: ':', outputDelimeter: ' ', fields: '0,3'});
+    ensure(splitted, output);
+  });
+
+  it('should be invokable from the shell', function () {
+    const input = 'one.,two.,three.,four';
+    const output = 'one four';
+    const splitted = exec(`echo "${input}" | ./split.js -i ., -f0,3`).toString().trim();
+    ensure(splitted, output);
   });
 });
+
+function ensure(splitted, output) {
+  assert (splitted === output,
+          `split: ${splitted} != out: ${output}`);
+}
