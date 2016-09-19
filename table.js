41735ced0e67efc40db54333ff9ea399998f7366
Hershal Bhave <hershal.bhave@gmail.com>
2016-09-19 10:02:50 -0500

    table: table is normalized to max entries

--- table.js
+++ table.js
@@ -8,14 +8,46 @@ const util = require('./util');
 
 const NewlineSerializer = require('./newline-serializer');
 
+class CLITable extends cliTable {
+  constructor() {
+    super(
+      {head: '', chars:
+       {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''}
+      });
+
+    // dictionary maps row length to row index
+    this.rowLengths = {};
+  }
+
+  push(row) {
+    const index = this.length;
+    super.push(row);
+    if (typeof this.rowLengths[row.length] == "undefined") {
+      this.rowLengths[row.length] = [index];
+    } else {
+      this.rowLengths[row.length].push(index);
+    }
+  }
+
+  normalize() {
+    const lengths = Object.keys(this.rowLengths);
+    const max = Math.max(...lengths);
+
+    for (const length of lengths) {
+      if (length == max) { continue; }
+      for (const rowIndex of this.rowLengths[length]) {
+        while (this[rowIndex].length < max) {
+          this[rowIndex].push('');
+        }
+      }
+    }
+  }
+}
+
 function main() {
   const options = util.decodeOptions(args);
   const serializer = new NewlineSerializer();
-
-  let table = new cliTable(
-    {head: '', chars:
-     {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''}
-    });
+  let table = new CLITable();
 
   process.stdin.on('data', (data) => {
     serializer.serialize(data, (line) => {
@@ -27,6 +59,7 @@ function main() {
     serializer.flush((line) => {
       processLine(line, table, options);
     });
+    table.normalize();
     console.log(table.toString());
   });
 }
